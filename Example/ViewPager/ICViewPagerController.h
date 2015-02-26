40b4828be3f949e4d526aa37496ece5237c01d44
Ilter Cengiz <iltercengiz@yahoo.com>
2015-02-26 10:21:45 +0200

    Added class 'ICViewPagerController'

---
+++ Example/ViewPager/ICViewPagerController.h
@@ -0,0 +1,13 @@
+//
+//  ICViewPagerController.h
+//  ViewPager
+//
+//  Created by Ilter Cengiz on 26/02/15.
+//  Copyright (c) 2015 Ilter Cengiz. All rights reserved.
+//
+
+#import "ViewPagerController.h"
+
+@interface ICViewPagerController : ViewPagerController
+
+@end
