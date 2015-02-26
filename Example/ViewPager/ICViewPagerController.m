366963876461134ac9584cd382085d274ee52864
Ilter Cengiz <iltercengiz@yahoo.com>
2015-02-26 15:25:13 +0200

    Example app is updated with data source implementation

--- Example/ViewPager/ICViewPagerController.m
+++ Example/ViewPager/ICViewPagerController.m
@@ -8,30 +8,79 @@
 
 #import "ICViewPagerController.h"
 
-@interface ICViewPagerController ()
+@interface ICViewPagerController () <ViewPagerControllerDataSource>
+
+@property (nonatomic) NSMutableArray *cache;
 
 @end
 
 @implementation ICViewPagerController
 
+#pragma mark - View life cycle
+
 - (void)viewDidLoad {
+    
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    self.title = @"View Pager";
+    
+    self.cache = [NSMutableArray arrayWithCapacity:10];
+    
+    for (NSInteger index = 0; index < 10; index++) {
+        [self.cache addObject:[NSNull null]];
+    }
+    
+    self.dataSource = self;
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
+#pragma mark - View pager controller data source
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (UIViewController *)viewPagerController:(ViewPagerController *)viewPagerController controllerForTabAtIndex:(NSUInteger)index {
+    
+    UIViewController *vc;
+    
+    if ([[self.cache objectAtIndex:index] isEqual:[NSNull null]]) {
+        vc = [self.storyboard instantiateViewControllerWithIdentifier:@"ContentViewController"];
+        
+        vc.view.backgroundColor = [UIColor colorWithWhite:244.0/255.0 alpha:1.0];
+        
+        UILabel *label = (UILabel *)[vc.view viewWithTag:1];
+        label.text = [NSString stringWithFormat:@"%li", index];
+    } else {
+        vc = [self.cache objectAtIndex:index];
+    }
+    
+    return vc;
+    
 }
-*/
+
+- (UIView *)viewPagerController:(ViewPagerController *)viewPagerController viewForTabAtIndex:(NSUInteger)index {
+    
+    UILabel *l;
+    
+    if ([[self.cache objectAtIndex:index] isEqual:[NSNull null]]) {
+        l = [UILabel new];
+        
+        l.backgroundColor = [UIColor colorWithWhite:192.0/255.0 alpha:1.0];
+        l.text = [NSString stringWithFormat:@"%li", index];
+        l.textAlignment = NSTextAlignmentCenter;
+        
+        [l sizeToFit];
+    } else {
+        l = [self.cache objectAtIndex:index];
+    }
+    
+    return l;
+    
+}
+
+- (NSInteger)numberOfTabsInViewPagerController:(ViewPagerController *)viewPagerController {
+    return 10;
+}
 
 @end
